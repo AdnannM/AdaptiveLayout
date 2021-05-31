7c05e0de4b40d7ae53fdc41d0d87081ab55285c7
AdnannM <48089787+AdnannM@users.noreply.github.com>
2021-05-31 15:16:02 +0200

    Practice Layout

---
+++ AdaptiveLayout/AdaptiveLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AdaptiveLayout
+//
+//  Created by Adnann Muratovic on 31.05.21.
+//  Copyright © 2021 Adnann Muratovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
